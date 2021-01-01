ab8d3a89c36a42add38e36eb226d8162ae3c59c3
Jon DeMaagd <jdemaagd7@outlook.com>
2021-01-01 15:10:32 -0500

    app arch starter

---
+++ guessit/README.md
@@ -0,0 +1,11 @@
+# Guess It: App Architecture
+
+## Think About
+
+- For the various features, what data is required to be displayed to the user?
+- Do the features require multiple states?
+  - Think about a social media app with the ability to edit or comment on the post.
+- What logic is required to make these features functional? Which layer do you think that logic would reside?
+- Are there any long running tasks that persist when the application is in the background?
+- Does the data update while you’re using the app? Do you have to do anything to make that change?
+
---
+++ guessit/src/main/java/com/jdemaagd/guessit/screens/TitleFragment.kt
@@ -0,0 +1,32 @@
+package com.jdemaagd.guessit.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+import com.jdemaagd.guessit.R
+import com.jdemaagd.guessit.databinding.FragmentTitleBinding
+
+/**
+ * Fragment for the starting or title screen of the app
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false)
+
+        binding.playGameButton.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleToGame())
+        }
+
+        return binding.root
+    }
+}
